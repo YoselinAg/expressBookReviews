dfde50c974fb4afdadfd395a91c8536bd62b5b91
YoselinAg <yoselinanabel@gmail.com>
2026-01-16 15:58:13 -0500

    edit auth token

--- final_project/index.js
+++ final_project/index.js
@@ -10,9 +10,26 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
+app.use("/customer/auth/*", function auth(req, res, next) {
+    if (!req.session || !req.session.authorization) {
+      return res.status(401).json({ message: "No autorizado: falta token" });
+    }
+  
+    const token = req.session.authorization.accessToken;
+    if (!token) {
+      return res.status(401).json({ message: "No autorizado: token no encontrado" });
+    }
+  
+    jwt.verify(token, "fingerprint_customer", (err, user) => {
+      if (err) {
+        return res.status(401).json({ message: "No autorizado: token inválido" });
+      }
+  
+      req.user = user;
+      next();
+    });
+  });
+  
  
 const PORT =5000;
 
