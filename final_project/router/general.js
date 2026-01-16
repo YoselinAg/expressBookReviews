1fc8ac02a9633f083e324a3f0bba2299a3583e60
YoselinAg <yoselinanabel@gmail.com>
2026-01-16 16:47:09 -0500

    task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,10 +28,23 @@ public_users.get('/isbn/:isbn', function (req, res) {
   
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+    let filteredBooks = {};
+  
+    Object.keys(books).forEach((key) => {
+      if (books[key].author === author) {
+        filteredBooks[key] = books[key];
+      }
+    });
+  
+    if (Object.keys(filteredBooks).length > 0) {
+      return res.status(200).send(JSON.stringify(filteredBooks, null, 4));
+    } else {
+      return res.status(404).json({ message: "No books found for this author" });
+    }
+  });
+  
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
